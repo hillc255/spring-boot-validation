5eb1e4f7556d9e1d056e4e13b59550e5dc1b4422
Claudia Hill <hillc255@yahoo.com>
2019-09-26 21:36:55 -0400

    Initial commit

---
+++ src/main/java/com/example/springbootvalidation/SpringBootValidationApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class SpringBootValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootValidationApplication.class, args);
+	}
+
+}
